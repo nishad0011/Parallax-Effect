2cfd3254a4d327c84b388fd5c29840a59b1619a6
nishad0011 <nishadgund@gmail.com>
2023-08-03 10:03:57 +0530

    Second commit

--- game.py
+++ game.py
@@ -17,17 +17,17 @@ font = pygame.font.Font("freesansbold.ttf", 32)
 
 #ASSETS
 background = [
-			[pygame.image.load("./800p/_11_background.png").convert_alpha()		,0,0,0],
-			[pygame.image.load("./800p/_10_distant_clouds.png").convert_alpha()	,0,0,1],
-			[pygame.image.load("./800p/_09_distant_clouds1.png").convert_alpha(),0,0,2],
-			[pygame.image.load("./800p/_08_clouds.png").convert_alpha()			,0,0,3],
-			[pygame.image.load("./800p/_07_huge_clouds.png").convert_alpha()	,0,0,4],
-			[pygame.image.load("./800p/_06_hill2.png").convert_alpha()			,0,0,5],
-			[pygame.image.load("./800p/_05_hill1.png").convert_alpha()			,0,0,6],
-			[pygame.image.load("./800p/_04_bushes.png").convert_alpha()			,0,0,7],
-			[pygame.image.load("./800p/_03_distant_trees.png").convert_alpha()	,0,0,8],
-			[pygame.image.load("./800p/_02_trees and bushes.png").convert_alpha(),0,0,9],
-			[pygame.image.load("./800p/_01_ground.png").convert_alpha()			,0,0,10],
+			[pygame.image.load("./800p/_11_background.png").convert_alpha()			,0,0,0],
+			[pygame.image.load("./800p/_10_distant_clouds.png").convert_alpha()		,0,0,1],
+			[pygame.image.load("./800p/_09_distant_clouds1.png").convert_alpha()	,0,0,2],
+			[pygame.image.load("./800p/_08_clouds.png").convert_alpha()				,0,0,3],
+			[pygame.image.load("./800p/_07_huge_clouds.png").convert_alpha()		,0,0,4],
+			[pygame.image.load("./800p/_06_hill2.png").convert_alpha()				,0,0,5],
+			[pygame.image.load("./800p/_05_hill1.png").convert_alpha()				,0,0,6],
+			[pygame.image.load("./800p/_04_bushes.png").convert_alpha()				,0,0,7],
+			[pygame.image.load("./800p/_03_distant_trees.png").convert_alpha()		,0,0,8],
+			[pygame.image.load("./800p/_02_trees and bushes.png").convert_alpha()	,0,0,9],
+			[pygame.image.load("./800p/_01_ground.png").convert_alpha()				,0,0,10],
 			]
 
 #GAME VARIABLES
